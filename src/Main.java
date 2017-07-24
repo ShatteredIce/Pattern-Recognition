7f4b4952c5cd1070a09e746f72535dd9f6644640
Natalie Dodson <kittenface48@gmail.com>
2017-07-24 11:41:36 -0700

    added greyscale

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 //i changed it!!
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
 public class Main {
+	
 	
 	public Main(){
 		BufferedImage test = loadImage("./res/test_triangle.jpg:");
@@ -26,8 +28,28 @@ public class Main {
 		}
 	}
 	
-	private convertGrayscale(){
-		
+	private BufferedImage convertGrayscale(BufferedImage test){
+		//pure image is the one that is greyscaled 
+		int picWidth = test.getWidth();
+    	int picHeight = test.getHeight();
+    	BufferedImage pureImage = new BufferedImage(picWidth, picHeight, test.getType()); ; //i think that this is a problem in the way the pointers work
+    	for (int counterX = 0; counterX < (int)(picWidth ) ; counterX += 1){
+    		for (int counterY = 0; counterY < (int)(picHeight ) ; counterY += 1){
+    			int originalColor;
+        		originalColor = test.getRGB(counterX, counterY);
+        		Color myColor = new Color(originalColor);
+        		int Red = myColor.getRed();
+        		int Blue = myColor.getBlue();
+        		int Green = myColor.getGreen();
+        		
+        		Color myNewColor = null;
+        		int Grey = (Red + Blue + Green) / 3;
+            	myNewColor = new Color (Grey, Grey, Grey);
+            	int newRGB = myNewColor.getRGB();
+        		pureImage.setRGB(counterX, counterY, newRGB);
+    		}
+    	}
+    	return pureImage;
 	}
-
+		
 }
