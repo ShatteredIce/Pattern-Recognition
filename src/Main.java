5060f6254fba8e462c5a0d25047a743ded853e73
ShatteredIce <icywind.katsuo@gmail.com>
2017-07-28 14:38:22 -0700

    pushed gaussian blur function

--- src/Main.java
+++ src/Main.java
@@ -20,45 +20,53 @@ public class Main {
 	Scanner scan = new Scanner(System.in);
 	
 	public Main(){
-//		BufferedImage test = loadImage("./res/raw/triangle3.png");
+		BufferedImage test = loadImage("./res/raw/outlet.jpg");
 //		storeImage(highlightShape(findEdges(convertGrayscale(test)), test), "./res/processed/triangle_out.png");
-		//storeImage(findEdges(convertGrayscale(test)), "./res/processed/intoutput.png");
+//		storeImage(findEdges(test), "./res/processed/edge.png");
+//		storeImage(findEdges(gaussianBlur(test)), "./res/processed/blur+edge.png");
+		BufferedImage out = test;
+		for (int i = 0; i < 3; i++) {
+			out = gaussianBlur(out);
+			System.out.println("iteration: " + i);
+		}
+		storeImage(out, "./res/processed/blur.png");
+		storeImage(findEdges(out), "./res/processed/superblur.png");
 		String input = "input";
 		int iter = 0;
 		int trainingIndex = 0;
-		NeuralNetwork net = new NeuralNetwork();
-		double[] data = new double[net.getNumInputs()];
-		
-		while(iter <= 1000000){
-			net.train(trainingIndex);
-			if(iter % 100000 == 0){
-				System.out.println("ITERATION: " + iter);
-				net.displayWeights();
-				net.displayResult(false);
-			}
-			iter++;
-			trainingIndex++;
-			if(trainingIndex == net.trainingData.length){
-				trainingIndex = 0;
-			}
-		}
-		
-		while(true){
-			System.out.println();
-			for(int i = 0; i < data.length; i++){
-				System.out.print("Enter data point " + (i+1) + ": ");
-				input = scan.nextLine();
-				try{
-					data[i] = Double.parseDouble(input);
-					System.out.println(data[i]);
-				}
-				catch (Exception e){
-					System.out.println("Enter a double!");
-					i--;
-				}
-			}
-			net.run(data);
-		}	
+//		NeuralNetwork net = new NeuralNetwork();
+//		double[] data = new double[net.getNumInputs()];
+//		
+//		while(iter <= 800000){
+//			net.train(trainingIndex);
+//			if(iter % 100000 == 0){
+//				System.out.println("ITERATION: " + iter);
+//				net.displayWeights();
+//				net.displayResult(false);
+//			}
+//			iter++;
+//			trainingIndex++;
+//			if(trainingIndex == net.trainingData.length){
+//				trainingIndex = 0;
+//			}
+//		}
+//		
+//		while(true){
+//			System.out.println();
+//			for(int i = 0; i < data.length; i++){
+//				System.out.print("Enter data point " + (i+1) + ": ");
+//				input = scan.nextLine();
+//				try{
+//					data[i] = Double.parseDouble(input);
+//					System.out.println(data[i]);
+//				}
+//				catch (Exception e){
+//					System.out.println("Enter a double!");
+//					i--;
+//				}
+//			}
+//			net.run(data);
+//		}	
 	}
 
 	public static void main(String[] args) {
@@ -134,6 +142,70 @@ public class Main {
 			}
 		}
 		return convertedImage;
+	}
+	
+	private BufferedImage gaussianBlur(BufferedImage img){
+		double sigma = 1;
+		int radius = 1;
+		int width = img.getWidth();
+		int height = img.getHeight();
+		BufferedImage convertedImage = new BufferedImage(width, height, img.getType()); 
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < radius; x++) {
+				convertedImage.setRGB(x, y, img.getRGB(x, y));
+			}
+			for (int x = width - radius; x < width; x++) {
+				convertedImage.setRGB(x, y, img.getRGB(x, y));
+			}
+		}
+		for (int x = radius; x < width - radius; x++) {
+			for (int y = 0; y < radius; y++) {
+				convertedImage.setRGB(x, y, img.getRGB(x, y));
+			}
+			for (int y = height - radius; y < height; y++) {
+				convertedImage.setRGB(x, y, img.getRGB(x, y));
+			}
+		}
+		for(int xpos = radius; xpos < width - radius; xpos++){
+			for(int ypos = radius; ypos < height - radius; ypos++){
+				double red = 0;
+				double blue = 0;
+				double green = 0;
+				double[][] weights = new double[2*radius + 1][2* radius + 1];
+				double sumWeights = 0;
+				Color[][] colors = new Color[2*radius + 1][2* radius + 1];
+				//find color and weight for the 3 * 3 box surrounding target pixel
+				for(int i = xpos - radius; i <= xpos + radius; i++){
+					for(int j = ypos - radius; j <= ypos + radius; j++){
+						double weight = gaussian(i - xpos, j - ypos, sigma);
+						weights[i - xpos + radius][j - ypos + radius] = weight;
+						sumWeights += weight;
+						colors[i - xpos + radius][j - ypos + radius] = new Color(img.getRGB(i, j));
+					}
+				}
+				//set sum of all weights to 1
+				for (int i = 0; i < weights.length; i++) {
+					for (int j = 0; j < weights[0].length; j++) {
+						weights[i][j]  /= sumWeights;
+					}
+				}
+				//get gaussian blur value of center point
+				for (int i = 0; i < weights.length; i++) {
+					for (int j = 0; j < weights[0].length; j++) {
+						red +=  weights[i][j] * colors[i][j].getRed();
+						green +=  weights[i][j] * colors[i][j].getGreen();
+						blue +=  weights[i][j] * colors[i][j].getBlue();
+					}
+				}
+				Color newColor = new Color((int) red, (int) green, (int) blue);
+				convertedImage.setRGB(xpos, ypos, newColor.getRGB());
+			}
+		}
+		return convertedImage;
+	}
+	
+	private double gaussian(double x, double y, double sigma){
+		return 1/(2*Math.PI*Math.pow(sigma, 2))*Math.exp(-(Math.pow(x, 2) + Math.pow(y, 2))/ 2*Math.pow(sigma, 2));
 	}
 	
 	private BufferedImage findEdges(BufferedImage img){
